080784b1fef6878f787871b79adda780449b6e43
Harry <harald.innetzberger@web.de>
2023-02-12 13:12:37 +0100

    fix: Themedefinition, remove color object.

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -5,18 +5,6 @@ import { mdi } from 'vuetify/iconsets/mdi-svg';
 
 const light: ThemeDefinition = {
   dark: false,
-  colors: {
-    background: '#FFFFFF',
-    surface: '#FFFFFF',
-    primary: '#6200EE',
-    'primary-darken-1': '#3700B3',
-    secondary: '#03DAC6',
-    'secondary-darken-1': '#018786',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00',
-  },
 };
 
 const dark: ThemeDefinition = {
